04550529bc203bbee022a92ab420676a2dbb9345
xingxianZhang <zxx895113853@gmail.com>
2018-10-16 09:01:17 +0800

    Update Leetcode/Easy/Two Sum.py
    finish

--- Leetcode/Easy/Two Sum.py
+++ Leetcode/Easy/Two Sum.py
@@ -15,4 +15,14 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        
+        num_to_index = {}
+        for i, num in enumerate(nums):
+            if target-num in num_to_index:
+                return [num_to_index[target-num], i]
+            num_to_index[num] = i
+
+if __name__ == '__main__':
+    nums = [2, 7, 11, 15]
+    target = 22
+    sum = Solution()
+    print(sum.twoSum(nums, target))
